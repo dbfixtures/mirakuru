779fa258fb1052089152c63dc0a5de612575519a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-05-04 16:25:51 -0400

    tests: pick free port instead of hardcoding to 3000
    3000 - or any other hardcoded port - may be occupied, which breaks the
    test.
    
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

--- tests/executors/test_tcp_executor.py
+++ tests/executors/test_tcp_executor.py
@@ -5,6 +5,8 @@ Some of these tests run ``nc``: when running Debian, make sure the
 """
 
 import logging
+import socket
+from typing import Any
 
 import pytest
 from _pytest.logging import LogCaptureFixture
@@ -12,16 +14,28 @@ from _pytest.logging import LogCaptureFixture
 from mirakuru import AlreadyRunning, TCPExecutor, TimeoutExpired
 from tests import HTTP_SERVER_CMD
 
-PORT = 7986
 
+# Allocate a random free port to avoid hardcoding and risking hitting an
+# occupied one; then let its number be reused elsewhere (by `nc`).
+def _find_free_port() -> Any:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("localhost", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+test_port = _find_free_port()
+
+
+PORT = 7986
 HTTP_SERVER = f"{HTTP_SERVER_CMD} {PORT}"
-NC_COMMAND = 'bash -c "sleep 2 && nc -lk 3000"'
+NC_COMMAND = f'bash -c "sleep 2 && nc -lk {test_port}"'
 
 
 def test_start_and_wait(caplog: LogCaptureFixture) -> None:
     """Test if executor await for process to accept connections."""
     caplog.set_level(logging.DEBUG, logger="mirakuru")
-    executor = TCPExecutor(NC_COMMAND, "localhost", port=3000, timeout=5)
+    executor = TCPExecutor(NC_COMMAND, "localhost", port=test_port, timeout=5)
     executor.start()
     assert executor.running() is True
     executor.stop()
@@ -29,7 +43,7 @@ def test_start_and_wait(caplog: LogCaptureFixture) -> None:
 
 def test_repr_and_str() -> None:
     """Check the proper str and repr conversion."""
-    executor = TCPExecutor(NC_COMMAND, "localhost", port=3000, timeout=5)
+    executor = TCPExecutor(NC_COMMAND, "localhost", port=test_port, timeout=5)
     # check proper __str__ and __repr__ rendering:
     assert "TCPExecutor" in repr(executor)
     assert NC_COMMAND in str(executor)
@@ -37,8 +51,8 @@ def test_repr_and_str() -> None:
 
 def test_it_raises_error_on_timeout() -> None:
     """Check if TimeoutExpired gets raised correctly."""
-    command = 'bash -c "sleep 10 && nc -lk 3000"'
-    executor = TCPExecutor(command, host="localhost", port=3000, timeout=2)
+    command = f'bash -c "sleep 10 && nc -lk {test_port}"'
+    executor = TCPExecutor(command, host="localhost", port=test_port, timeout=2)
 
     with pytest.raises(TimeoutExpired):
         executor.start()
